c043a56e8a157d71c615e5fdce6d3d9ee578af0f
Lucas Ramos <lucas_ramos95@outlook.com>
2022-08-15 18:15:06 -0300

    6th Commit - Formulário finalizado + Css

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,16 +1,13 @@
-import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto/CampoTexto';
-import './componentes/CampoTexto/CampoTexto.css'
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
-      <CampoTexto label="Nome"/>
-      <CampoTexto label="Cargo"/>
-      <CampoTexto label="Imagem"/>
+      <Formulario/>
     </div>
-  );
+  )
 }
 
 export default App;
--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,12 +1,12 @@
 import './Banner.css'
 
-function Banner() {
-    //JSX
+export const Banner = () => {
+    // JSX
     return (
         <header className="banner">
-            <img src="./imagens/banner.png" alt="O banner principal da página."/>
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
 
-export default Banner
+export default Banner;
