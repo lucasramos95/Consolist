0b95e4c7c9e68c8724afcac4cdfffb7f126c3735
Lucas Ramos <lucas_ramos95@outlook.com>
2022-08-29 22:44:45 -0300

    10th Commit - Map do card criado + Dados do card

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
@@ -6,6 +7,9 @@ const Time = (props) => {
     return (
         <section className="time" style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>
         </section>
     )
 }
